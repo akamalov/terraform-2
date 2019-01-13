f8f489fe3322d2649c44f09d6d1ddb6c51d57b0f
omer <omerb@codevalue.net>
2019-01-13 22:55:08 +0200

    reverted before subnet

--- aks/main.tf
+++ aks/main.tf
@@ -11,7 +11,7 @@ terraform {
 }
 
 locals {
-  resource_group_name = "${var.customer}-${terraform.workspace}"      # dynamic build of RG name
+  resource_group_name = "${var.customer}-${terraform.workspace}"   # dynamic build of RG name
   default_secret_name = "${terraform.workspace}-secret"
   create_resource     = "${terraform.workspace == "default" ? 0 : 1}" # condition to prevent creating resources with a "default" terraform workspace
 }
@@ -48,7 +48,7 @@ module "container_registry" {
   source          = "github.com/jungopro/terraform-modules.git?ref=dev/azure/container_registry"
   create_resource = "${local.create_resource}"
   name            = "${replace("${local.resource_group_name}acr", "-", "")}"
-  resource_group  = "${module.resource_group.resource_group_name}"
+  resource_group  = "${local.resource_group_name}"
   location        = "${var.location}"
   sa_id           = "${element("${module.storage_account.id}", 0)}"
   tags            = "${merge("${var.tags}", map("terraform workspace", "${terraform.workspace}"), map("customer", "${var.customer}"))}"
@@ -62,24 +62,4 @@ module "vnet" {
   cidr                = "${var.vnet_cidr}"
   location            = "${var.location}"
   tags                = "${merge("${var.tags}", map("terraform workspace", "${terraform.workspace}"), map("customer", "${var.customer}"))}"
-}
-
-module "backend_subnet" {
-  source            = "github.com/jungopro/terraform-modules.git?ref=dev/azure/subnet"
-  create_resource   = "${local.create_resource}"
-  name              = "${module.vnet.vnet_name}-backend-subnet"
-  resource_group    = "${module.resource_group.resource_group_name}"
-  address_prefix    = "${var.backend_address_prefix}"
-  vnet_name         = "${module.vnet.vnet_name}"
-  service_endpoints = "${var.backend_endpoints}"
-}
-
-module "frontend_subnet" {
-  source            = "github.com/jungopro/terraform-modules.git?ref=dev/azure/subnet"
-  create_resource   = "${local.create_resource}"
-  name              = "${module.vnet.vnet_name}-frontend-subnet"
-  resource_group    = "${module.resource_group.resource_group_name}"
-  address_prefix    = "${var.frontend_address_prefix}"
-  vnet_name         = "${module.vnet.vnet_name}"
-  service_endpoints = "${var.frontend_endpoints}"
 }
--- aks/variables.tf
+++ aks/variables.tf
@@ -1,6 +1,6 @@
 variable "location" {
   description = "The default Azure region for the resource provisioning"
-  default     = "West Europe"
+  default = "West Europe"
 }
 
 variable "customer" {
@@ -13,7 +13,6 @@ variable "tags" {
 
 variable "storage_account" {
   type = "map"
-
   default = {
     "name" = ""
   }
@@ -21,24 +20,4 @@ variable "storage_account" {
 
 variable "vnet_cidr" {
   description = "The vnet CIDR in classful format"
-}
-
-variable "backend_address_prefix" {
-  description = "The address prefix to use for the subnet in classful format"
-}
-
-variable "backend_endpoints" {
-  description = "The list of Service endpoints to associate with the subnet. Possible values include: Microsoft.AzureActiveDirectory, Microsoft.AzureCosmosDB, Microsoft.EventHub, Microsoft.KeyVault, Microsoft.ServiceBus, Microsoft.Sql and Microsoft.Storage"
-  type = "list"
-  default = []
-}
-
-variable "frontend_address_prefix" {
-  description = "The address prefix to use for the subnet in classful format"
-}
-
-variable "frontend_endpoints" {
-  description = "The list of Service endpoints to associate with the subnet. Possible values include: Microsoft.AzureActiveDirectory, Microsoft.AzureCosmosDB, Microsoft.EventHub, Microsoft.KeyVault, Microsoft.ServiceBus, Microsoft.Sql and Microsoft.Storage"
-  type = "list"
-  default = []
 }
