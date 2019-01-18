359129d3225e5c0abacc2d94d24f3e1f47ebf7b4
omer <omerb@codevalue.net>
2019-01-18 17:07:50 +0200

    using new rg module

--- aks/main.tf
+++ aks/main.tf
@@ -22,25 +22,15 @@ locals {
 
 module "resource_group" {
   source              = "github.com/jungopro/terraform-modules.git?ref=dev/azure/resource_group"
-  count               = 2
+  count               = "${local.create_resource}"
   resource_group_name = "${local.resource_group_name}"
   location            = "${var.location}"
   tags                = "${merge("${var.tags}", map("terraform workspace", "${terraform.workspace}"), map("customer", "${var.customer}"))}"
 }
 
-resource "random_id" "randomId_sa" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group = "${module.resource_group.resource_group_name}"
-  }
-
-  byte_length = 8
-}
-
 module "storage_account" {
   source                   = "github.com/jungopro/terraform-modules.git?ref=dev/azure/storage_account"
-  create_resource          = "${local.create_resource}"
-  name                     = "${var.storage_account["name"] != "" ? "${lower("${replace("${var.storage_account["name"]}", "-", "")}")}" : "sa${random_id.randomId_sa.hex}" }"
+  count                    = 2
   resource_group           = "${module.resource_group.resource_group_name}"
   location                 = "${var.location}"
   account_tier             = "${var.storage_account["account_tier"]}"
