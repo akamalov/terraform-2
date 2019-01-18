d9e11bd3ac2c910e93569e0e9318350778825f92
omer <omerb@codevalue.net>
2019-01-18 20:52:14 +0200

    modified rg ref

--- aks/main.tf
+++ aks/main.tf
@@ -30,8 +30,8 @@ module "resource_group" {
 
 module "storage_account" {
   source                   = "github.com/jungopro/terraform-modules.git?ref=dev/azure/storage_account"
-  count                    = 2
-  resource_group           = "${module.resource_group.resource_group_name}"
+  count                    = 1
+  resource_group           = "${element("${module.resource_group.resource_group_name}", 0)}"
   location                 = "${var.location}"
   account_tier             = "${var.storage_account["account_tier"]}"
   account_replication_type = "${var.storage_account["account_replication_type"]}"
@@ -40,9 +40,9 @@ module "storage_account" {
 
 module "container_registry" {
   source          = "github.com/jungopro/terraform-modules.git?ref=dev/azure/container_registry"
-  create_resource = "${local.create_resource}"
+  count = "${local.create_resource}"
   name            = "${replace("${local.resource_group_name}acr", "-", "")}"
-  resource_group  = "${module.resource_group.resource_group_name}"
+  resource_group  = "${element("${module.resource_group.resource_group_name}", 0)}"
   location        = "${var.location}"
   sa_id           = "${element("${module.storage_account.id}", 0)}"
   tags            = "${merge("${var.tags}", map("terraform workspace", "${terraform.workspace}"), map("customer", "${var.customer}"))}"
@@ -52,7 +52,7 @@ module "vnet" {
   source              = "github.com/jungopro/terraform-modules.git?ref=dev/azure/vnet"
   create_resource     = "${local.create_resource}"
   name                = "${module.resource_group.resource_group_name}-primary-vnet"
-  resource_group_name = "${module.resource_group.resource_group_name}"
+  resource_group_name = "${element("${module.resource_group.resource_group_name}", 0)}"
   cidr                = "${var.vnet_cidr}"
   location            = "${var.location}"
   tags                = "${merge("${var.tags}", map("terraform workspace", "${terraform.workspace}"), map("customer", "${var.customer}"))}"
@@ -62,7 +62,7 @@ module "backend_subnet" {
   source            = "github.com/jungopro/terraform-modules.git?ref=dev/azure/subnet"
   create_resource   = "${local.create_resource}"
   name              = "${module.vnet.vnet_name}-backend-subnet"
-  resource_group    = "${module.resource_group.resource_group_name}"
+  resource_group    = "${element("${module.resource_group.resource_group_name}", 0)}"
   address_prefix    = "${var.backend_address_prefix}"
   vnet_name         = "${module.vnet.vnet_name}"
   service_endpoints = "${var.backend_endpoints}"
@@ -72,7 +72,7 @@ module "frontend_subnet" {
   source            = "github.com/jungopro/terraform-modules.git?ref=dev/azure/subnet"
   create_resource   = "${local.create_resource}"
   name              = "${module.vnet.vnet_name}-frontend-subnet"
-  resource_group    = "${module.resource_group.resource_group_name}"
+  resource_group    = "${element("${module.resource_group.resource_group_name}", 0)}"
   address_prefix    = "${var.frontend_address_prefix}"
   vnet_name         = "${module.vnet.vnet_name}"
   service_endpoints = "${var.frontend_endpoints}"
